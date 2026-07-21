fc604665a6e13189b25cd8d6025fb7196f43b36b
CousinsAJC <137661572+CousinsAJC@users.noreply.github.com>
2026-07-20 23:09:23 -0400

    create grid, organize code
    organize code to be more readable.  Create function to draw the game grid.  Not in final build but helps debugging wall bumps and rotation errors.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 import { blockArray, block, i, j, l, o, s, t, z } from "./blocks.js";
 
+
+// -- Get DOM Elements
 const canvas = document.getElementById("game");
 const context = canvas.getContext("2d");
 const startButton = document.getElementById("start-button");
@@ -8,49 +10,84 @@ startButton.addEventListener('click', e=>{
 })
 let isRunning = false;
 
-let px = 1
-let py = 1
-let ps = 1
+
+// -- Declare Variables
+const blockSize = 20;
+const gridX = 310;
+const gridY = 100;
 
 
 
-function update(){
-    px = px + ps;
-    py = px + ps;
-}
-
-function draw(){
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = "blue";
-    context.fillRect(px, py, 10, 10);
-
-}
-
-function printToCanvas(text){
-    context.font = "36px Arial";
-    context.fillStyle = "green";
-    context.textAlign = "center";
-    context.fillText(text, canvas.width/2, canvas.height/2);
-}
-
-
-// ------------------------
-
+// -- Set game speed to ~ 60FPS & start game
 setInterval(gameLoop, 17);
-
 gameLoop();
 
-// ------------------------
 
 
 
+
+// -- Define Functions
 function gameLoop(){
     if (isRunning){
         update();
         draw();
     }
     else {
-        printToCanvas("Press Start to Begin");
+        printToCenterScreen("Press Start to Begin", "green");
     }
 
 }
+
+function update(){
+    //testUpdate();
+}
+
+function draw(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    //testDraw();
+    drawGridToCanvas();
+}
+
+function printToCenterScreen(text, color){
+    context.font = "36px Arial";
+    context.fillStyle = color;
+    context.textAlign = "center";
+    context.fillText(text, canvas.width/2, canvas.height/2);
+}
+
+function drawGridToCanvas(){
+    for (let i = 0; i < 10; i++){
+        for (let j = 0; j < 20; j++){
+            context.fillStyle = "white";
+            context.fillRect(gridX + i * blockSize, gridY + j * blockSize, 1, 1)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -- Test Functions and Variables
+function testUpdate(){
+    px = px + ps;
+    py = px + ps;
+}
+
+function testDraw(){
+    context.fillStyle = "blue";
+    context.fillRect(px, py, 10, 10);
+}
+
+let px = 1
+let py = 1
+let ps = 1
+// -- Test Functions and Variables
