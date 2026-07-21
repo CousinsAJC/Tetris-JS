26debe5c614c39f64d200a4e9311ddcc5f9c0698
CousinsAJC <137661572+CousinsAJC@users.noreply.github.com>
2026-07-20 22:04:10 -0400

    create game loop
    create game loop with initial load, update and draw functions.  test functionality by moving box across screen.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,56 @@
 const canvas = document.getElementById("game");
 const context = canvas.getContext("2d");
+const startButton = document.getElementById("start-button");
+startButton.addEventListener('click', e=>{
+    isRunning = true;
+})
 let isRunning = false;
 
+let px = 1
+let py = 1
+let ps = 1
 
 
 
-function update(dt){
+
+
+setInterval(gameLoop, 17);
+
+load();
+gameLoop();
+
+
+function gameLoop(){
+    if (isRunning){
+        update();
+        draw();
+    }
+    else {
+        printToCanvas("Press Start to Begin");
+    }
+
+}
+
+function load(){
     
 }
+
+function update(){
+    px = px + ps;
+    py = px + ps;
+}
+
+function draw(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "blue";
+    context.fillRect(px, py, 10, 10);
+
+}
+
+function printToCanvas(text){
+    context.font = "36px Arial";
+    context.fillStyle = "green";
+    context.textAlign = "center";
+    context.fillText(text, canvas.width/2, canvas.height/2);
+}
+
